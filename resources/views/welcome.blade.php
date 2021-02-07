75394c5a9ecacb70093b265725ece82bef86ea50
Timur <borodatimur@gmail.com>
2021-02-07 16:15:38 +0300

    Открыл индексацию

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <title>@yield('meta_title')</title>
     <meta name="description" content="@yield('meta_description')">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="robots" content="noindex,nofollow">
+    <meta name="robots" content="index,follow">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <base href="{{url('/')}}">
